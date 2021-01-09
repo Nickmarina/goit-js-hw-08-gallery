dcd8266d77719e2b05d95761a7228b7d0ecf9f19
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2021-01-09 21:14:49 +0200

    Устраняет баги с разметкой

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 // 3. сделать делегирование по галерее
 // 4. Модальное окно
 
-import galleryItems from './gallery-items';
+import imgs from './gallery-items.js';
 
-const galleryRef = document.querySelector('.js - gallery');
+const galleryRef = document.querySelector('.js-gallery');
 const modalRef = document.querySelector('.js-lightbox');
 const overlayRef = document.querySelector('.lightbox__overlay');
 const modalImgRef = document.querySelector('.lightbox__image');
 const closeBtnRef = modalRef.querySelector('[data-action="close-lightbox"]');
 
-const items = galleryItems.map(item => {
+const items = imgs.map(item => {
   const li = document.createElement('li');
   li.classList.add('gallery__item');
   const a = document.createElement('a');
@@ -22,10 +22,10 @@ const items = galleryItems.map(item => {
   img.classList.add('gallery__image');
   img.setAttribute('src', item.preview);
   img.setAttribute('data-source', item.original);
-  img.setAttribute('data-index', galleryItems.indexOf(item));
+  img.setAttribute('data-index', imgs.indexOf(item));
   img.setAttribute('alt', item.description);
   a.appendChild(img);
   return li;
 });
 
-gallery.append(...items);
+galleryRef.append(...items);
